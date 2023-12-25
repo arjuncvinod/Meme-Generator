b3bd08e55ffc8e32ed19efb897c2bc9eac953704
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-12-25 17:11:23 +0530

    header.jsx updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,6 @@ export default function Header() {
         className="header--image"
        alt=""/>
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
